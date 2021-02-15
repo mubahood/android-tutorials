95f3a61c47b1101f2c42a3a04527282defa0537b
Muhindo Mubaraka <30492534+iut-cse-4301-mubahood@users.noreply.github.com>
2021-02-15 16:58:10 +0600

    first

--- app/src/main/java/com/muadhi/customfonts/MainActivity.java
+++ app/src/main/java/com/muadhi/customfonts/MainActivity.java
@@ -1,14 +1,21 @@
 package com.muadhi.customfonts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView dynamic_font;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dynamic_font = findViewById(R.id.dynamic_font);
+        Typeface my_font = ResourcesCompat.getFont(this, R.font.bad_script);
     }
 }
